c0690d640680f7577a46a045a7e3f97c859b1beb
Nelson Ramirez <xhyskoax@gmail.com>
2023-03-02 19:48:37 -0300

    configuración inicial

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 import express from 'express';
+import {v4 as uuid} from 'uuid';
+import cors from 'cors';
+import {create} from 'express-handlebars';
+import fileUpload from 'express-fileupload';
+
+import * as path from "path";
+import { fileURLToPath } from "url";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
-app.listen(3001, () => console.log("http://localhost:3001"));
+app.listen(3000, () => console.log("http://localhost:3000"));
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+app.use(fileUpload({
+    limits: { fileSize: 5 * 1024 * 1024 },
+    abortOnLimit: true,
+    responseOnLimit: "La imágen que está subiendo sobrepasa los 5mb permitidos."
+
+  }));
+  app.use(cors());
+
+
+//configuracion de handlebars
+
+const hbs = create({
+	partialsDir: [
+		"views/partials/",
+	],
+});
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", path.resolve(__dirname, "./views"));
